7165d36d109bce639aa765e0d5fba3c2445167c9
EsipovPA <esipov.p@mail.ru>
2020-07-26 21:03:15 +0300

    Inserting artists separately

--- main.py
+++ main.py
@@ -9,12 +9,12 @@ from ScanConsert import get_concert_list, get_concert_object
 from MySqlInterface import MySqlConn
 
 # Debug special character processing using url: https://www.afisha.ru/concert/2013655/
-# debug_meta = get_concert_object('https://www.afisha.ru/concert/2013655/').to_json()
+# debug_meta = get_concert_object('https://www.afisha.ru/concert/2012335/').to_json()
 # print(f'debug meta = {debug_meta}')
 
 
 # Get concert metadata
-concert_list = get_concert_list(pages_limit=10)
+concert_list = get_concert_list(pages_limit=20)
 
 # Insert all concerts
 print(f'concert count = {len(concert_list)}')
@@ -24,7 +24,6 @@ for link in concert_list:
     c_obj = get_concert_object(concert_link=link)
     MySqlConn().store_concert(concert_meta=c_obj.to_json())
     counter += 1
-
 
 # Insert single concert
 # first_concert = get_concert_object(concert_link=concert_list[0])
